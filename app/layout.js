21096611c49ee7ed3be023e3d688b8a6298d6109
PIET Thomas <thomas.piet@ynov.com>
2024-06-18 12:05:07 +0200

    progress on page CONTACT

--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,26 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-import Header from '@/components/Header'
+import Header from '@/components/Header';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Blog - Thomas PIET',
   description: 'Ynov Toulouse Campus',
 }
 
-export default function RootLayout ({ children }) {
+export default function RootLayout({ children }) {
   return (
     <html>
-
-
     <body>
-    <Header/>
-    <div className={inter.className}>
-      {children}</div>
+    <Header />
+    <div className="main-content">
+      <div className={inter.className}>
+        {children}
+      </div>
+    </div>
     </body>
-
     </html>
-  )
+  );
 }
